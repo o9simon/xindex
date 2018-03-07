34fa737ef2bafb0a37632b3b5ba49d19ec3beea6
o9simon <simonricgir@gmail.com>
2018-03-07 10:47:54 -0500

    Revert "Add random avatars feature"
    This reverts commit 19205e4378daded13c7e5b56170a3af450b2c913.

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 {{{appJS}}}
 
 // This script is embedded in the footer of every page
-var avatarElements = document.getElementsByClassName('avatar');
-if (avatarElements.length > 0) {
-  var avatarCount = 10;
-  var random = Math.floor(Math.random() * avatarCount) + 1;
-  avatarElements[0].setAttribute('src', '/_Avatars/' + random + '.png');
-}
